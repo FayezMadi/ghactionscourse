3e0dbdd42a1b8d563ff5ef628b4a5f23703cc0e9
FayezMadi <fayez.madi@sitech.me>
2026-08-25 14:03:46 +0300

    Creates a new NodeJS app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function greet(name)
+{
+    return `Hello, ${name}!`;
+} // <--- Close the greet function here
+
+// Now these run when the file is loaded by Node:
+module.exports = greet;
+
+if (require.main === module)
+{
+    console.log(greet("World"));
+}
