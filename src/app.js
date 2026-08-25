7ecb988ae60119d857c4ce122e2aa7c294254f6e
FayezMadi <fayez.madi@sitech.me>
2026-08-25 14:19:59 +0300

    Test changes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ function greet(name)
 // Now these run when the file is loaded by Node:
 module.exports = greet;
 
-if (require.main === module)
-{
-    console.log(greet("World"));
-}
+//if (require.main === module)
+//{
+//    console.log(greet("World"));
+//}
