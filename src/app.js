19cb9ea022f69f659fc8d591539c0fb4ea63450c
FayezMadi <fayez.madi@sitech.me>
2026-08-25 14:27:06 +0300

    new try with a comment

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ function greet(name)
 
 // Now these run when the file is loaded by Node:
 module.exports = greet;
-
-//if (require.main === module)
-//{
-//    console.log(greet("World"));
-//}
+//Added new commit to check things
+if (require.main === module)
+{
+    console.log(greet("World"));
+}
